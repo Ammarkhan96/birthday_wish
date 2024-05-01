d437e16a961f4d66d639a4ebf7387a580eb1ca97
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-05-01 15:38:06 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Birthday from './Birthday';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RouterBirthday from './RouterBirthday';
+import Generate from './Generate';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Birthday />} />
+          <Route exact path='/birthday/:name?/:day?/:month?' element={<RouterBirthday />} />
+          <Route exact path='/generate' element={<Generate />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
